c4a327cd892914dd53c98fc4756b71f3ea094472
Vigrimi <tamotd@ya.ru>
2021-03-26 14:01:32 +0300

    26march#1

--- src/lesson5HoWoStrings/Lesson5HoWorkStrings.java
+++ src/lesson5HoWoStrings/Lesson5HoWorkStrings.java
@@ -43,11 +43,45 @@ public class Lesson5HoWorkStrings
             } else i--;
         }
         String[] arrStr1Output = Arrays.copyOf(arrStr1, fallInArr);
-        System.out.println(Arrays.toString(arrStr1Output));
+        System.out.println("вывести получившийся массив в консоль: " + Arrays.toString(arrStr1Output));
 
       /*  2. Найти количество вхождений одной строки в другую.
             Например, строка "дом" встречается в строке "дом домик домой одомашненный" 4 раза */
-        String str2 = "f";
+        System.out.println("\n2. Найти количество вхождений одной строки в другую.\nвведи исходный текст: ");
+        String inpTextNull = scanner.nextLine();
+        String inpText = scanner.nextLine();
+        //System.out.println("1: " + inpText);
+        System.out.println("введи искомую фразу: ");
+        String etalonText = scanner.nextLine();
+        int intsEtalonTextLen = etalonText.length();
+        //System.out.println("2: " + etalonText);
+        String inpTextBezProbelov = inpText.replaceAll(" ", "");
+        //System.out.println("3: " + inpTextBezProbelov);
+        char[] vseSimvoly = inpTextBezProbelov.toCharArray();
+        int textLen = inpTextBezProbelov.length();
+        int qtySovpadeniy = 0;
+        for (int i = 0; i < (textLen - (intsEtalonTextLen - 1)); i++)
+        {
+            String sravni = new String(vseSimvoly, i, intsEtalonTextLen);
+            //System.out.println("4: " + sravni);
+            if (etalonText.equals(sravni)) qtySovpadeniy++;
+        }
+        System.out.println("количество вхождений одной строки в другую = " + qtySovpadeniy + " раз(а).");
+
+        /*3. Написать функцию, которая проверяет, является ли строка палиндромом.
+            Палиндром — это число, буквосочетание, слово или текст, которые одинаково читаются
+            по буквам или по словам как слева направо, так и справа налево.
+        Например, 202 - палиндром / fafaf - палиндром / а роза упала на лапу Азора - палиндром*/
+        System.out.println("\n3. Написать функцию, которая проверяет, является ли строка палиндромом.\nвведи текст для проверки: ");
+        String inpTextPalindrom = scanner.nextLine();
+        inpTextPalindrom = inpTextPalindrom.replaceAll("[^A-Za-zА-яа-я0-9]", "");
+        StringBuilder strBuiNaProverku = new StringBuilder(inpTextPalindrom);
+        strBuiNaProverku = strBuiNaProverku.reverse();
+        String strNaProverku = strBuiNaProverku.toString ();
+        System.out.println(strNaProverku.equalsIgnoreCase(inpTextPalindrom) ? "да, введённое - это палиндром!" : "нет, введённое - это НЕ палиндром!");
+
+        /*4. Заменить все буквы в слове на строчные, а первую букву на заглавную
+        Например, дано hello, получаем Hello / дано HeLLO, получаем Hello*/
 
 
     }
