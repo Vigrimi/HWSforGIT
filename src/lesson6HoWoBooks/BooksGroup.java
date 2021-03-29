67a7ddb8b47a3a92dbc668565a83dddeaa9d6ec2
Vigrimi <tamotd@ya.ru>
2021-03-29 17:40:41 +0300

    #6

--- src/lesson6HoWoBooks/BooksGroup.java
+++ src/lesson6HoWoBooks/BooksGroup.java
@@ -6,25 +6,45 @@ import java.util.Objects;
 
 public class BooksGroup
 {
+//    Books nameOfBooks1 = new Books();
+//    nameOfBooks1.;
+
     //Books books;
     public String/*Books*/[] arrBooks = new String /*Books*/[20];; //создали массив
-
+    //int a = 0;
     public void addBook (String /*Books*/ book)
     {
+        int a = 0;
         for (int i = 0; i < 20; i++)
         {
+            /*if (arrBooks[i] != null) a++;
+            else*/
             if (arrBooks[i] == null)
             {
-                arrBooks[i] = book;
+                /*arrBooks[i] = book;
                 return;
+            } else */
+                for (int j = 0; j < (i-1); j++)
+                {
+                    String frazaProverka = book.substring(28, book.length() - 54);
+                    if (arrBooks[j].contains(frazaProverka)) a++; // если такая книга есть чота ошибку выдаёт
+                    // Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.contains(java.lang.CharSequence)" because "this.arrBooks[j]" is null
+                }
+
+                if (a == 0)
+                {
+                    arrBooks[i] = book;
+                    return;
+                }
+
             }
         }
     }
 
     @Override
     public String toString() {
-        return "BooksGroup{" +
-                "arrBooks=" + Arrays.toString(arrBooks) +
-                '}';
+        return "Наша библиотека " +
+                "состоит из следующих книг: \n" + Arrays.toString(arrBooks) +
+                ".\n";
     }
 }
--- src/lesson6HoWoBooks/Lesson6HoWorkBooksMain.java
+++ src/lesson6HoWoBooks/Lesson6HoWorkBooksMain.java
@@ -11,38 +11,34 @@ public class Lesson6HoWorkBooksMain
         //Scanner scanner = new Scanner(System.in);
         System.out.println("1. Реализовать объектную модель: КНИГИ. ");
         int max = 20;
+        BooksGroup booksGroup = new BooksGroup();
         for (int i = 0; i < max; i++)
         {
 
             Author authorNameAndSurname = new Author();
             authorNameAndSurname.getAuthorNameAndSurname();
 
-
             Books books = new Books(); // можно ли как-то вызвать одну команду, чтобы она спрашивала все характеристики?
             books.getNameOfBook(); // если нажать пустой энтер, почему нет НАЛа?
             books.getQtyPages(); /* ??? */
             books.getTypeYear();
 
-
             Warehouse warehouse = new Warehouse();
             warehouse.setUuid();
 
-            System.out.print("№" + (i + 1) + ") " + authorNameAndSurname);
-            System.out.println(books);
-            System.out.println(warehouse);
+//            System.out.print("№" + (i + 1) + ") " + authorNameAndSurname);
+//            System.out.println(books);
+//            System.out.println(warehouse);
 
             StringBuilder sb = new StringBuilder();
-            sb = sb.append(authorNameAndSurname).append(books).append(warehouse);
+            sb = sb.append("№" + (i+1) + ") ").append(authorNameAndSurname).append(books).append(warehouse);
             //System.out.println("1516... " + sb);
             String books1 = sb.toString();
             //books = books1;
 
-            BooksGroup booksGroup = new BooksGroup();
+            //BooksGroup booksGroup = new BooksGroup();
             booksGroup.addBook(books1); //(booksGroup.arrBooks[i]);
             System.out.println(booksGroup);
         }
-
-
-
     }
 }
