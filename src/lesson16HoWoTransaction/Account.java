361a442d94fc6610bbf8edeea6b2af6cbaf1bdc9
Vigrimi <tamotd@ya.ru>
2021-04-28 16:27:43 +0300

    #1

---
+++ src/lesson16HoWoTransaction/Account.java
@@ -0,0 +1,38 @@
+package lesson16HoWoTransaction;
+
+public class Account
+{
+    private String number;
+    private long balance;
+
+    public Account(String number, long balance)
+    {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    /*@Override
+    public String toString()
+    {
+        return "Account{" +
+                "number='" + number + '\'' +
+                ", balance=" + balance +
+                '}';
+    }*/
+}
---
+++ src/lesson16HoWoTransaction/Transaction.java
@@ -0,0 +1,56 @@
+package lesson16HoWoTransaction;
+
+public class Transaction
+{
+    private String uuid;
+    private long sum;
+    private Account account;
+
+    public Transaction(String uuid, long sum, Account account)
+    {
+        this.uuid = uuid;
+        this.sum = sum;
+        this.account = account;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    public void setSum(long sum) {
+        this.sum = sum;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public String getAccountToString()
+    {
+        String accountToString = this.account.toString();
+        return accountToString;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "uuid='" + uuid + '\'' +
+                ", sum=" + sum +
+                ", account=" + /*getAccountToString() +*/ account +
+                '}';
+    }
+}
