583516d44c78d6540d09ffa208c6f0a96d43537a
Vigrimi <tamotd@ya.ru>
2021-06-23 20:09:56 +0300

    #1

---
+++ HWSforGIT_spring/src/main/java/exam3/multi/Client1Starting.java
@@ -0,0 +1,11 @@
+package exam3.multi;
+
+import java.io.IOException;
+
+public class Client1Starting
+{
+    public static void main(String[] args) throws IOException {
+        new NetworkClientSide().run();
+    }
+
+}
